e05378847c232e22d3932b60d6e48822a2ee9b68
Marcus Pitz <marcusviniciuspitz@gmail.com>
2019-12-05 20:08:24 -0300

    Base spring boot

---
+++ src/main/java/com/simple/rest/api/JavaSprintSimpleRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.simple.rest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSprintSimpleRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSprintSimpleRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simple/rest/api/JavaSprintSimpleRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simple.rest.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSprintSimpleRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
